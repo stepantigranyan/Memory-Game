72d28a7be8a743d681b3f2040fc614a6c3e9acf3
Stepan Tigranyan <stepantigranyan9@gmail.com>
2026-09-23 23:03:55 +0400

    I changed cards shuffle function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Settings from './settings/settings.js';
-import allModeButtons from "./buttons/buttons.js";
+import allModeButtons from './buttons/buttons.js';
 
 const startButton = document.getElementById('start-button');
 const stopButton = document.getElementById('stop-button');
@@ -21,175 +21,182 @@ let timerInterval;
 // =================================================================
 
 function flipCard(e) {
-    const isRunning = settings.getRunning();
-    const locked = settings.getLocked();
+  const isRunning = settings.getRunning();
+  const locked = settings.getLocked();
 
-    if (!isRunning || locked) return;
+  if (!isRunning || locked) return;
 
-    const card = e.currentTarget;
-    card.classList.add('rotate-y-[180deg]');
+  const card = e.currentTarget;
+  card.classList.add('rotate-y-[180deg]');
 
-    const { row, column } = card.dataset;
+  const { row, column } = card.dataset;
 
-    const firstCard = settings.getFirstCard();
-    const secondCard = settings.getSecondCard();
+  const firstCard = settings.getFirstCard();
+  const secondCard = settings.getSecondCard();
 
-    const allCards = settings.getCards();
+  const allCards = settings.getCards();
 
-    setTimeout(() => {
-        card.innerHTML = allCards[row][column].body;
-    }, 200);
+  setTimeout(() => {
+    card.innerHTML = allCards[row][column].body;
+  }, 200);
 
-    if (!firstCard) {
-        settings.setFirstCard(card);
-        return;
-    }
+  if (!firstCard) {
+    settings.setFirstCard(card);
+    return;
+  }
 
-    if (!secondCard) {
-        settings.setSecondCard(card);
-    }
+  if (!secondCard) {
+    settings.setSecondCard(card);
+  }
 
-
-    settings.setLocked(true);
-    matchCards(settings.getFirstCard(), settings.getSecondCard(), allCards);
+  settings.setLocked(true);
+  matchCards(settings.getFirstCard(), settings.getSecondCard(), allCards);
 }
 
 function matchCards(firstCard, secondCard, allCards) {
-    if (firstCard === secondCard) {
-        setTimeout(() => {
-            settings.clearFirstAndSecondCards();
-            settings.setLocked(false);
-        }, 500);
-        return;
-    }
+  if (firstCard === secondCard) {
+    setTimeout(() => {
+      settings.clearFirstAndSecondCards();
+      settings.setLocked(false);
+    }, 500);
+    return;
+  }
 
-    const firstCardRow = +firstCard.dataset.row;
-    const firstCardColumn = +firstCard.dataset.column;
+  const firstCardRow = +firstCard.dataset.row;
+  const firstCardColumn = +firstCard.dataset.column;
 
-    const secondCardRow = +secondCard.dataset.row;
-    const secondCardColumn = +secondCard.dataset.column;
+  const secondCardRow = +secondCard.dataset.row;
+  const secondCardColumn = +secondCard.dataset.column;
 
-    if (
-        allCards[firstCardRow][firstCardColumn].id ===
-        allCards[secondCardRow][secondCardColumn].id
-    ) {
-        checkWin(firstCard, secondCard);
-    } else {
-        setTimeout(() => {
-            settings.clearFirstAndSecondCards();
-            settings.setLocked(false);
-        }, 500);
-    }
+  if (
+    allCards[firstCardRow][firstCardColumn].id ===
+    allCards[secondCardRow][secondCardColumn].id
+  ) {
+    checkWin(firstCard, secondCard);
+  } else {
+    setTimeout(() => {
+      settings.clearFirstAndSecondCards();
+      settings.setLocked(false);
+    }, 500);
+  }
 }
 
 function checkWin(firstCard, secondCard) {
-    settings.incrementCheckedCards();
+  settings.incrementCheckedCards();
 
-    const { rows, columns } = settings.getSizes();
-    const checkedCardsCount = settings.getCheckedCardsCount();
+  const { rows, columns } = settings.getSizes();
+  const checkedCardsCount = settings.getCheckedCardsCount();
 
-    if (checkedCardsCount === (rows * columns) / 2) {
-        endGame("Win");
-    } else {
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-        settings.setFirstCard(null);
-        settings.setSecondCard(null);
-        settings.setLocked(false);
-    }
+  if (checkedCardsCount === (rows * columns) / 2) {
+    endGame('Win');
+  } else {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    settings.setFirstCard(null);
+    settings.setSecondCard(null);
+    settings.setLocked(false);
+  }
 }
 
 // Win and Lose
 // =================================================================
 
 function endGame(message) {
-    let seconds = 0;
-    showWin(message);
+  let seconds = 0;
+  showWin(message);
 
-    const messageInterval = setInterval(() => {
-        if(seconds === 4) {
-            clearInterval(messageInterval);
-            reset();
-        } else if(seconds % 2) {
-            gameMessage.classList.remove('text-white');
-            gameMessage.classList.add('text-rose-900');
-        } else {
-            gameMessage.classList.remove('text-rose-900');
-            gameMessage.classList.add('text-white');
-        }
-        seconds++;
-    }, 1000);
+  const messageInterval = setInterval(() => {
+    if (seconds === 4) {
+      clearInterval(messageInterval);
+      reset();
+    } else if (seconds % 2) {
+      gameMessage.classList.remove('text-white');
+      gameMessage.classList.add('text-rose-900');
+    } else {
+      gameMessage.classList.remove('text-rose-900');
+      gameMessage.classList.add('text-white');
+    }
+    seconds++;
+  }, 1000);
 }
 
 // Timer
 // =================================================================
 
 function updateTimer() {
-    const timer = settings.getTimer();
-    const mins = Math.floor(timer / 60);
-    const secs = timer - mins * 60;
-    return `${mins > 0 ? `0${mins}` : '00'}:${secs < 10 ? `0${secs}` : `${secs}`}`;
+  const timer = settings.getTimer();
+  const mins = Math.floor(timer / 60);
+  const secs = timer - mins * 60;
+  return `${mins > 0 ? `0${mins}` : '00'}:${secs < 10 ? `0${secs}` : `${secs}`}`;
 }
 
 // Draw Functions
 // =================================================================
 
 function drawBoard() {
-    removeBoard();
-    const { rows, columns } = settings.getSizes();
+  removeBoard();
+  const { rows, columns } = settings.getSizes();
 
-    const board = document.createElement('div');
-    board.setAttribute('id', 'board');
-    board.classList.add('grid', 'gap-2', `grid-cols-${columns}`, 'p-5', 'border', 'border-cyan-900', 'rounded-md');
+  const board = document.createElement('div');
+  board.setAttribute('id', 'board');
+  board.classList.add(
+    'grid',
+    'gap-2',
+    `grid-cols-${columns}`,
+    'p-5',
+    'border',
+    'border-cyan-900',
+    'rounded-md',
+  );
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            const card = document.createElement('div');
-            card.classList.add(
-                'w-15',
-                'h-15',
-                'cursor-pointer',
-                'bg-rose-800',
-                'flex',
-                'justify-center',
-                'items-center',
-                'text-white',
-                'text-md',
-                'rounded-md',
-                'shadow-xl/40',
-                'transition-all',
-                'duration-400',
-                '[transform-style:preserve-3d]',
-            );
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const card = document.createElement('div');
+      card.classList.add(
+        'w-15',
+        'h-15',
+        'cursor-pointer',
+        'bg-rose-800',
+        'flex',
+        'justify-center',
+        'items-center',
+        'text-white',
+        'text-md',
+        'rounded-md',
+        'shadow-xl/40',
+        'transition-all',
+        'duration-400',
+        '[transform-style:preserve-3d]',
+      );
 
-            card.setAttribute('data-row', `${i}`);
-            card.setAttribute('data-column', `${j}`);
-            card.addEventListener('click', flipCard);
+      card.setAttribute('data-row', `${i}`);
+      card.setAttribute('data-column', `${j}`);
+      card.addEventListener('click', flipCard);
 
-            board.append(card);
-        }
+      board.append(card);
     }
+  }
 
-    boardContainer.append(board);
-    timerContainer.innerText = updateTimer();
+  boardContainer.append(board);
+  timerContainer.innerText = updateTimer();
 }
 
 function drawModeButtons() {
-    allModeButtons.forEach((button) => {
-        button.addClick(() => {
-            settings = Settings.create(button.rows, button.columns);
-            startButton.disabled = false;
-        });
-
-        modeButtonsContainer.append(button.getElement());
+  allModeButtons.forEach((button) => {
+    button.addClick(() => {
+      settings = Settings.create(button.rows, button.columns);
+      startButton.disabled = false;
     });
+
+    modeButtonsContainer.append(button.getElement());
+  });
 }
 
 function removeBoard() {
-    const board = document.getElementById('board');
-    if (board) {
-        board.remove();
-    }
+  const board = document.getElementById('board');
+  if (board) {
+    board.remove();
+  }
 }
 
 // Reset
@@ -197,89 +204,88 @@ function removeBoard() {
 // =================================================================
 
 function reset() {
+  allModeButtons.forEach((button) => {
+    const radioInput = button.getElement().children[0];
+    radioInput.checked = false;
+  });
 
-    allModeButtons.forEach(button => {
-        const radioInput = button.getElement().children[0];
-        radioInput.checked = false;
-    })
+  settings = null;
 
-    settings = null;
+  stopButton.classList.add('hidden');
+  continueButton.classList.add('hidden');
 
-    stopButton.classList.add('hidden');
-    continueButton.classList.add('hidden');
+  startButton.classList.remove('hidden');
+  startButton.disabled = true;
 
-    startButton.classList.remove('hidden');
-    startButton.disabled = true;
+  resetButton.classList.add('hidden');
+  resetButton.disabled = true;
 
-    resetButton.classList.add('hidden');
-    resetButton.disabled = true;
+  timerContainer.innerText = '';
+  gameMessage.innerText = '';
 
-    timerContainer.innerText = '';
-    gameMessage.innerText = '';
+  modeButtons.classList.remove('hidden');
 
-    modeButtons.classList.remove('hidden');
-
-    clearInterval(timerInterval);
-    removeBoard();
+  clearInterval(timerInterval);
+  removeBoard();
 }
 
 function showWin(message) {
-    gameMessage.innerText = `You ${message}!`;
-    modeButtons.classList.add('hidden');
-    stopButton.classList.add('hidden');
-    resetButton.classList.add('hidden');
-    timerContainer.innerText = '';
-    clearTimeout(timerInterval);
-    removeBoard();
+  gameMessage.innerText = `You ${message}!`;
+  modeButtons.classList.add('hidden');
+  stopButton.classList.add('hidden');
+  resetButton.classList.add('hidden');
+  timerContainer.innerText = '';
+  clearTimeout(timerInterval);
+  removeBoard();
 }
 
 // Start
 // =================================================================
 
 function start() {
-    drawModeButtons();
+  drawModeButtons();
 
-    startButton.addEventListener('click', (e) => {
-        e.currentTarget.classList.add('hidden');
-        stopButton.classList.remove('hidden');
-        settings.setRunning(true);
-        modeButtons.classList.add('hidden');
-        resetButton.classList.remove('hidden');
-        drawBoard();
+  startButton.addEventListener('click', (e) => {
+    e.currentTarget.classList.add('hidden');
+    stopButton.classList.remove('hidden');
+    settings.setRunning(true);
+    modeButtons.classList.add('hidden');
+    resetButton.classList.remove('hidden');
+    drawBoard();
 
-        timerInterval = setInterval(() => {
-            settings.decrementTimer();
-            timerContainer.innerText = updateTimer(settings.getTimer());
-            if(!settings.getTimer()) {
-                endGame("Lose");
-            }
-        }, 1000);
-    });
+    timerInterval = setInterval(() => {
+      settings.decrementTimer();
+      timerContainer.innerText = updateTimer(settings.getTimer());
+      if (!settings.getTimer()) {
+        endGame('Lose');
+      }
+    }, 1000);
+  });
 
-    stopButton.addEventListener('click', (e) => {
-        e.currentTarget.classList.add('hidden');
-        continueButton.classList.remove('hidden');
-        settings.setRunning(false);
-        resetButton.disabled = false;
-        clearInterval(timerInterval);
-    });
+  stopButton.addEventListener('click', (e) => {
+    e.currentTarget.classList.add('hidden');
+    continueButton.classList.remove('hidden');
+    settings.setRunning(false);
+    resetButton.disabled = false;
+    clearInterval(timerInterval);
+  });
 
-    continueButton.addEventListener('click', (e) => {
-        e.currentTarget.classList.add('hidden');
-        stopButton.classList.remove('hidden');
-        settings.setRunning(true);
-        resetButton.disabled = true;
+  continueButton.addEventListener('click', (e) => {
+    e.currentTarget.classList.add('hidden');
+    stopButton.classList.remove('hidden');
+    settings.setRunning(true);
+    resetButton.disabled = true;
 
-        timerInterval = setInterval(() => {
-            settings.decrementTimer();
-            timerContainer.innerText = updateTimer(settings.getTimer());
-            if(!settings.getTimer()) {
-                endGame("Lose");
-            }
-        }, 1000);
-    })
+    timerInterval = setInterval(() => {
+      settings.decrementTimer();
+      timerContainer.innerText = updateTimer(settings.getTimer());
+      if (!settings.getTimer()) {
+        endGame('Lose');
+      }
+    }, 1000);
+  });
 
-    resetButton.addEventListener('click', reset);
+  resetButton.addEventListener('click', reset);
 }
 
 // =================================================================
--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,116 +1,116 @@
-import {ALL_CARDS} from '../consts/const.js';
+import { ALL_CARDS } from '../consts/const.js';
 
 function shuffleCards(cards) {
-    return cards.sort(() => Math.random() - 0.5);
+  return cards.sort(() => Math.random() - 0.5);
 }
 
 function getCards(rows, columns) {
-    const shuffledCards = shuffleCards(ALL_CARDS);
-    const halfCards = shuffledCards.slice(0, (rows * columns) / 2);
-    const cards = [...halfCards, ...halfCards];
-    const matrix = [];
+  const shuffledCards = shuffleCards(ALL_CARDS);
+  const halfCards = shuffledCards.slice(0, (rows * columns) / 2);
+  const cards = shuffleCards([...halfCards, ...halfCards]);
+  const matrix = [];
 
-    for (let i = 0; i < rows; i++) {
-        const matrixRow = [];
-        for (let j = 0; j < columns; j++) {
-            matrixRow.push(cards[columns * i + j]);
-        }
-        matrix.push(matrixRow);
+  for (let i = 0; i < rows; i++) {
+    const matrixRow = [];
+    for (let j = 0; j < columns; j++) {
+      matrixRow.push(cards[columns * i + j]);
     }
+    matrix.push(matrixRow);
+  }
 
-    return matrix;
+  return matrix;
 }
 
 class Settings {
-    #cards;
-    #rows;
-    #columns;
-    #timer;
-    #checkedCardsCount;
+  #cards;
+  #rows;
+  #columns;
+  #timer;
+  #checkedCardsCount;
 
-    constructor(rows, columns) {
-        this.#cards = getCards(rows, columns);
-        this.#rows = rows;
-        this.#columns = columns;
-        this.#timer = Math.floor(rows * columns * 4);
+  constructor(rows, columns) {
+    this.#cards = getCards(rows, columns);
+    this.#rows = rows;
+    this.#columns = columns;
+    this.#timer = Math.floor(rows * columns * 4);
 
-        this.firstCard = null;
-        this.secondCard = null;
+    this.firstCard = null;
+    this.secondCard = null;
 
-        this.locked = false;
-        this.isRunning = false;
+    this.locked = false;
+    this.isRunning = false;
 
-        this.#checkedCardsCount = 0;
-    }
+    this.#checkedCardsCount = 0;
+  }
 
-    static create(rows, columns) {
-        return new Settings(rows, columns);
-    }
+  static create(rows, columns) {
+    return new Settings(rows, columns);
+  }
 
-    getCards() {
-        return this.#cards;
-    }
+  getCards() {
+    return this.#cards;
+  }
 
-    getSizes() {
-        return { rows: this.#rows, columns: this.#columns };
-    }
+  getSizes() {
+    return { rows: this.#rows, columns: this.#columns };
+  }
 
-    getTimer() {
-        return this.#timer;
-    }
+  getTimer() {
+    return this.#timer;
+  }
 
-    decrementTimer() {
-        this.#timer--;
-    }
+  decrementTimer() {
+    this.#timer--;
+  }
 
-    getCheckedCardsCount() {
-        return this.#checkedCardsCount;
-    }
+  getCheckedCardsCount() {
+    return this.#checkedCardsCount;
+  }
 
-    incrementCheckedCards() {
-        this.#checkedCardsCount++;
-    }
+  incrementCheckedCards() {
+    this.#checkedCardsCount++;
+  }
 
-    getFirstCard() {
-        return this.firstCard;
-    }
+  getFirstCard() {
+    return this.firstCard;
+  }
 
-    setFirstCard(value) {
-        this.firstCard = value;
-    }
+  setFirstCard(value) {
+    this.firstCard = value;
+  }
 
-    getSecondCard() {
-        return this.secondCard;
-    }
+  getSecondCard() {
+    return this.secondCard;
+  }
 
-    setSecondCard(value) {
-        this.secondCard = value;
-    }
+  setSecondCard(value) {
+    this.secondCard = value;
+  }
 
-    clearFirstAndSecondCards() {
-        this.firstCard.classList.remove('rotate-y-[180deg]');
-        this.secondCard.classList.remove('rotate-y-[180deg]');
-        this.firstCard.innerHTML = '';
-        this.secondCard.innerHTML = '';
-        this.firstCard = null;
-        this.secondCard = null;
-    }
+  clearFirstAndSecondCards() {
+    this.firstCard.classList.remove('rotate-y-[180deg]');
+    this.secondCard.classList.remove('rotate-y-[180deg]');
+    this.firstCard.innerHTML = '';
+    this.secondCard.innerHTML = '';
+    this.firstCard = null;
+    this.secondCard = null;
+  }
 
-    getLocked() {
-        return this.locked;
-    }
+  getLocked() {
+    return this.locked;
+  }
 
-    setLocked(value) {
-        this.locked = value;
-    }
+  setLocked(value) {
+    this.locked = value;
+  }
 
-    getRunning() {
-        return this.isRunning;
-    }
+  getRunning() {
+    return this.isRunning;
+  }
 
-    setRunning(value) {
-        this.isRunning = value;
-    }
+  setRunning(value) {
+    this.isRunning = value;
+  }
 }
 
 export default Settings;
